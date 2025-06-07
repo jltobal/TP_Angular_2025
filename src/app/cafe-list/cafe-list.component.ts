9a82b73256bda5b4977713621e1beec24764e623
jltobal <ziba.tobal@gmail.com>
2025-06-07 18:51:37 -0300

    inicio de proyecto

---
+++ src/app/cafe-list/cafe-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cafe-list',
+  imports: [],
+  templateUrl: './cafe-list.component.html',
+  styleUrl: './cafe-list.component.css'
+})
+export class CafeListComponent {
+
+}
